a020268cc064a919a650a947340d8c5fdb828a4b
= <carlitosxd2006@gmail.com>
2025-04-09 11:51:21 -0600

    Actualizando este ejercicio a que funcione completamente

---
+++ Figura20-20_19796703/Inicio.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Figura20_20_19796703
+{
+	public partial class Inicio : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+			Session["intCategoryID"] = txtCategory.Text;
+			Response.Redirect("SqlDataSourceClassSessionParameter.aspx");
+        }
+    }
+}
